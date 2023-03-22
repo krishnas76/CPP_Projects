8f56c5dc8a0980b3f901a17aeaa5a8cf684577eb
Srikanth Srinivasan <srikanthsrinivasan@Srikanths-MacBook-Pro-306.local>
2023-03-22 09:57:48 -0700

    final

--- BSTree/main.cpp
+++ BSTree/main.cpp
@@ -21,6 +21,7 @@ Node* del(Node* tree, int num);
 
 int main() {
   cout << "This is a Binary Search Tree." << endl;
+  //take in number of numbers
   int size;
   cout << "How many numbers would you like to enter?" << endl;
   cin >> size;
@@ -51,6 +52,7 @@ int main() {
     cin.getline(input, 80, '\n'); //take in user command
 
     if (strcmp(input, "add") == 0) {
+      //take in number of numbers
       int count;
       cout << "How many numbers would you like to add?" << endl;
       cin >> count;
@@ -62,42 +64,51 @@ int main() {
 	cin >> newnumbers[i];
       }
       cin.ignore(50, '\n');
+      //add all numbers to tree
       for (int i = 0; i < count; i++) {
 	add(tree, newnumbers[i]);
       }
     }
 
     else if (strcmp(input, "remove") == 0) {
-      cout << "Enter a number to delete:" << endl;
+      //take in number to remove
+      cout << "Enter a number to remove:" << endl;
       int number;
       cin >> number;
       cin.ignore(50, '\n');
+      //if number exists, remove it
       if (search(tree, number)) {
-	//delete
-        cout << number << " deleted from tree." << endl;
+	del(tree, number);
+        cout << number << " removed from tree." << endl;
       }
+      //if number doesn't exist
       else {
         cout << number << " not found in tree." << endl;
       }
     }
 
     else if (strcmp(input, "print") == 0) {
+      //print tree
       print(tree, 0);
     }
 
     else if (strcmp(input, "search") == 0) {
+      //take in number to search
       cout << "Enter a number to search for:" << endl;
       int number;
       cin >> number;
       cin.ignore(50, '\n');
+      //if number found
       if (search(tree, number)) {
 	cout << number << " found in tree." << endl;
       }
+      //number not found
       else {
 	cout << number << " not found in tree." << endl;
       }
     }
 
+    //quit
     else if (strcmp(input, "q") == 0) {
       break;
     }
@@ -112,20 +123,26 @@ int main() {
 }
 
 void add(Node* tree, int num) {
+  //if num greater than tree value
   if (num > tree->data) {
+    //if no right child, make a right child
     if (tree->right == nullptr) {
       Node* node = new Node(num);
       tree->right = node;
     }
+    //if there is right child, add to that child
     else {
       add(tree->right, num);
     }
   }
+  //num less than or equal to tree value
   else {
+    //if no left child, make a left child
     if (tree->left == nullptr) {
       Node* node = new Node(num);
       tree->left = node;
     }
+    //if there is left child, add to that child
     else {
       add(tree->left, num);
     }
@@ -150,12 +167,15 @@ void print(Node* tree, int space) {
 
 bool search(Node* tree, int num) {
   if (tree != nullptr) {
+    //if number found, return true
     if (tree->data == num) {
       return true;
     }
+    //if number greater than tree data, search in right child
     if (num > tree->data) {
       return search(tree->right, num);
     }
+    //if number less than or equal to tree data, search in left child
     else {
       return search(tree->left, num);
     }
@@ -164,16 +184,46 @@ bool search(Node* tree, int num) {
 }
 
 Node* del(Node* tree, int num) {
+  //if null, return tree
   if (tree == nullptr) {
     return tree;
   }
+  //if number greater than tree data, look to delete in right child
   if (num > tree->data) {
     tree->right = del(tree->right, num);
   }
+  //if number less than tree data, look to delete in left child
   else if (num < tree->data) {
     tree->left = del(tree->left, num);
   }
+  //number is equal to data, delete this node
   else {
-    
+    //if node has no child
+    if (tree->left == nullptr && tree->right == nullptr) {
+      return nullptr;
+    }
+    //if node only has left child
+    else if (tree->right == nullptr) {
+      Node* temp = tree->left;
+      delete tree;
+      return temp;
+    }
+    //if node only has right child
+    else if (tree->left == nullptr) {
+      Node* temp = tree->right;
+      delete tree;
+      return temp;
+    }
+    //node has left and right child
+    Node* temp = tree->right;
+    //find the node with the next greatest number
+    while (temp->left != nullptr) {
+      temp = temp->left;
+    }
+    //set tree data to that new number
+    tree->data = temp->data;
+    //delete the node with the next greatest number
+    tree->right = del(tree->right, temp->data);
   }
+  return tree;
 }
